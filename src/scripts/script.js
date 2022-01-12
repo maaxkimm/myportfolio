3c892652c091124b6e85d6612e8ac30a516db4e8
Max Kim <maxkim@berkeley.edu>
2022-01-11 19:10:09 -0800

    Added typing effect and education section, formatting issues

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,3 +3,58 @@ function Home() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+const text = document.getElementById("typing1");
+const phrases = [
+  "Welcome to my online portfolio, made by me! :)",
+  "I used HTML/CSS and Javascript, and I'm currently working on adding a back-end using Django",
+  "I enjoy building things!",
+];
+
+let one = 0;
+let two = 0;
+let currentPhrase = [];
+let del = false;
+let end = false;
+
+function loop() {
+  end = false;
+  text.innerHTML = currentPhrase.join("");
+
+  if (one < phrases.length) {
+    if (!del && two <= phrases[one].length) {
+      currentPhrase.push(phrases[one][two]);
+      two++;
+      text.innerHTML = currentPhrase.join("");
+    }
+
+    if (del && two <= phrases[one].length) {
+      currentPhrase.pop(phrases[one][two]);
+      two--;
+      text.innerHTML = currentPhrase.join("");
+    }
+
+    if (two == phrases[one].length) {
+      end = true;
+      del = true;
+    }
+
+    if (del && two === 0) {
+      currentPhrase = [];
+      del = false;
+      one++;
+      if (one === phrases.length) {
+        one = 0;
+      }
+    }
+  }
+
+  const fast = 50;
+  const normal = 100;
+
+  //if we're deleting, we want to speed up (fast) --> otherwise we'll use normal speed
+  const time = end ? 2000 : del ? fast : normal;
+  setTimeout(loop, time);
+}
+
+loop();
